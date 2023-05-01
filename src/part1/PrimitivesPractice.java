570fd8644c059aa94207ab4cfd144800db7a9a1e
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2023-05-01 10:54:56 -0400

    Java Bootcamp Part 1

---
+++ src/part1/PrimitivesPractice.java
@@ -0,0 +1,26 @@
+package part1;
+
+public class PrimitivesPractice {
+
+    public static void main(String[] args) {
+
+        // DataType var;
+
+        int a;
+        a = 10;
+        System.out.println(a);
+
+        double b = a; // implicit casting
+
+        System.out.println(b);
+
+        long c = 200;
+
+        short d = (short)c;
+
+
+
+
+    }
+
+}
---
+++ src/part1/WrapperClasses.java
@@ -0,0 +1,25 @@
+package part1;
+
+public class WrapperClasses {
+
+    public static void main(String[] args) {
+
+        char a = 'A';
+
+        Character b =  a;
+
+        int c = 25;
+
+        Integer d = c;
+
+
+        Boolean l = false;
+
+        boolean k = l;
+
+
+
+
+    }
+
+}
